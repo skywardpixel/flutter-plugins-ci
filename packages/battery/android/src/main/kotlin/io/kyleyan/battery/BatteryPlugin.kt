af00097a284ec321edf8b114b03d945ae32223d9
Kyle Yan <kyleyan@cs.washington.edu>
2020-07-22 08:46:18 -0700

    update android platform code

--- packages/battery/android/src/main/kotlin/io/kyleyan/battery/BatteryPlugin.kt
+++ packages/battery/android/src/main/kotlin/io/kyleyan/battery/BatteryPlugin.kt
@@ -4,12 +4,6 @@
 package io.kyleyan.battery
 
 import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.BatteryManager
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.plugin.common.BinaryMessenger
@@ -23,6 +17,7 @@ class BatteryPlugin : MethodCallHandler, FlutterPlugin {
 
     private var applicationContext: Context? = null
     private var methodChannel: MethodChannel? = null
+    var batteryLevel: BatteryLevel? = null
 
     override fun onAttachedToEngine(binding: FlutterPluginBinding) {
         onAttachedToEngine(binding.applicationContext, binding.binaryMessenger)
@@ -30,7 +25,8 @@ class BatteryPlugin : MethodCallHandler, FlutterPlugin {
 
     private fun onAttachedToEngine(applicationContext: Context, messenger: BinaryMessenger) {
         this.applicationContext = applicationContext
-        methodChannel = MethodChannel(messenger, "io.kyleyan.example/battery")
+        batteryLevel = BatteryLevel(applicationContext)
+        methodChannel = MethodChannel(messenger, "example.kyleyan.io/battery")
         methodChannel!!.setMethodCallHandler(this)
     }
 
@@ -42,7 +38,7 @@ class BatteryPlugin : MethodCallHandler, FlutterPlugin {
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         if (call.method == "getBatteryLevel") {
-            val batteryLevel = batteryLevel
+            val batteryLevel = batteryLevel!!.batteryLevel
             if (batteryLevel != -1) {
                 result.success(batteryLevel)
             } else {
@@ -52,21 +48,6 @@ class BatteryPlugin : MethodCallHandler, FlutterPlugin {
             result.notImplemented()
         }
     }
-
-    private val batteryLevel: Int
-        get() {
-            var batteryLevel = -1
-            batteryLevel = if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-                val batteryManager = applicationContext!!.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-                batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-            } else {
-                val intent = ContextWrapper(applicationContext)
-                        .registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-                (intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100
-                        / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1))
-            }
-            return batteryLevel
-        }
 
     companion object {
         /** Plugin registration.  */
