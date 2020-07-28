fded1f3a4ad7b41560d0f9d023731db3dad9c7ce
Kyle Yan <kyleyan@cs.washington.edu>
2020-07-28 11:35:24 -0700

    skip test if not exist

--- .circleci/test_all_plugins.sh
+++ .circleci/test_all_plugins.sh
@@ -2,29 +2,36 @@
 
 cd packages || exit
 
-for plugin in */; do
-    cd "$plugin" || exit
+for plugin_dir in */; do
+    cd "$plugin_dir" || exit
+    plugin=$(basename "$plugin_dir")
     case $1 in
         unit-test)
-            echo "Running Flutter unit tests for $(basename "$plugin")"
+            echo "=== Running Flutter unit tests for $plugin ==="
             flutter test
             ;;
         android-test)
-            echo "Running Android unit tests for $(basename "$plugin")"
+            echo "=== Running Android unit tests for $plugin ==="
             cd example/android || continue
             flutter build apk
             ./gradlew testDebugUnitTest
             cd ../..
             ;;
         ios-test)
-            echo "Running iOS unit tests for $(basename "$plugin")"
-            cd example/ios || continue
-            flutter build ios --no-codesign
-            xcodebuild test -workspace Runner.xcworkspace -scheme Runner -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.6'
-            cd ../..
+            echo "=== Running iOS unit tests for $plugin ==="
+            if [ -d "ios/Tests" ]; then
+                XCODEBUILD_DESTINATION="'platform=iOS Simulator,name=iPhone 11,OS=13.6'"
+                cd example/ios || continue
+                flutter build ios --no-codesign
+                xcodebuild test -workspace Runner.xcworkspace -scheme Runner -destination "$XCODEBUILD_DESTINATION"
+                cd ../..
+            else
+                echo "iOS unit tests for $plugin don't exist. Skipping."
+            fi
             ;;
     esac
     cd ..
+    echo
 done
 
 cd ..
