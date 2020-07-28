20d7ee3d2ba81e10aa480ea9dd16ee4c11b1c907
Kyle Yan <kyleyan@cs.washington.edu>
2020-07-27 17:00:38 -0700

    add doc comment, bump version

--- packages/battery/CHANGELOG.md
+++ packages/battery/CHANGELOG.md
@@ -1,3 +1,11 @@
+## 0.0.2+1-unstable
+
+* Add Dart doc comment to `battery.dart`.
+
+## 0.0.2-unstable
+
+* Add author email or website info.
+
 ## 0.0.1
 
 * TODO: Describe initial release.
--- packages/battery/lib/battery.dart
+++ packages/battery/lib/battery.dart
@@ -7,16 +7,15 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:meta/meta.dart' show visibleForTesting;
 
-enum BatteryState {
-  full,
-  charging,
-  discharging
-}
+enum BatteryState { full, charging, discharging }
 
+/// A plugin for reading the current battery level.
 class Battery {
+  /// Returns a Battery instance.
   factory Battery() {
     if (_instance == null) {
-      final MethodChannel methodChannel = const MethodChannel('example.kyleyan.io/battery');
+      final MethodChannel methodChannel =
+          const MethodChannel('example.kyleyan.io/battery');
       _instance = Battery.private(methodChannel);
     }
     return _instance;
@@ -29,6 +28,7 @@ class Battery {
 
   final MethodChannel _methodChannel;
 
+  /// The current battery level of the device.
   Future<int> get batteryLevel => _methodChannel
       .invokeMethod<int>('getBatteryLevel')
       .then<int>((dynamic result) => result);
