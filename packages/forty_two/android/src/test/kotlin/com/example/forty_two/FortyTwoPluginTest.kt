e96464225212055034ccb3c9cc5e95c39f5623c7
Kyle Yan <kyleyan@cs.washington.edu>
2020-07-29 13:15:28 -0700

    remove failed test in forty_two

--- packages/forty_two/android/src/test/kotlin/com/example/forty_two/FortyTwoPluginTest.kt
+++ packages/forty_two/android/src/test/kotlin/com/example/forty_two/FortyTwoPluginTest.kt
@@ -6,10 +6,4 @@ class FortyTwoPluginTest {
     fun onTestRun() {
         print("Hello, this is a test")
     }
-
-    @Test
-    fun onTestRun_fail() {
-        print("Hello, this is a failing test")
-        assertTrue(false)
-    }
 }
